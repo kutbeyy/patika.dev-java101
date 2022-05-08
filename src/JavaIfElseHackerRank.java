b90c470d96b74f07009183652a7b201018386482
Fatih ÇELİK <info.fatihcelik@gmail.com>
2022-05-08 10:01:33 +0300

    Java If-Else HackerRank Ödevi Yapıldı. https://www.hackerrank.com/info_fatihcelik

---
+++ src/JavaIfElseHackerRank.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+public class JavaIfElseHackerRank {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        if (N % 2 == 0) {
+
+            if (N >= 2 && N <= 5) {
+                System.out.println("Not Weird");
+            } else if (N >= 6 && N <= 20) {
+                System.out.println("Weird");
+            } else if (N > 20) {
+                System.out.println("Not Weird");
+            }
+
+        } else {
+            System.out.println("Weird");
+        }
+
+        scanner.close();
+    }
+}
