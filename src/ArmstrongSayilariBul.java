198cb8e6b53bd83184306e401e27857ea23681bc
Fatih ÇELİK <info.fatihcelik@gmail.com>
2022-05-11 12:00:38 +0300

    Armstrong Sayıları Bulan Program www.patika.dev

---
+++ src/ArmstrongSayilariBul.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ArmstrongSayilariBul {
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int number, temp, lastDigit, total = 0;
+
+
+        System.out.print("Enter a number: ");
+        number = temp = scan.nextInt(); // number sayısını temp değişkeninde yedekliyoruz
+
+        int digitNumber = (int) (Math.floor(Math.log10(number)) + 1); // exponent(üs) > sayı kaç haneli
+
+        for (int i = 1; i <= digitNumber; i++) {
+            lastDigit = number % 10; // last digit || Sayının son hanesi
+            total += (int) Math.pow(lastDigit, digitNumber);
+            number /= 10; // sayıyı 10'a bölerek son haneyi buluyoruz
+        }
+
+        if (temp == total) {
+            System.out.println("Number is Armstrong Number");
+        } else {
+            System.out.println("Number isn't Armstrong Number");
+        }
+
+        scan.close();
+    }
+
+}
