c673e01150d2a89d09e3a5fba6da0743dff9b763
Fatih ÇELİK <info.fatihcelik@gmail.com>
2022-05-11 12:08:59 +0300

    Bir sayının rakamları toplamını bulma www.patika.dev

--- src/ArmstrongSayilariBul.java
+++ src/ArmstrongSayilariBul.java
@@ -5,7 +5,7 @@ public class ArmstrongSayilariBul {
 
     public static void main(String[] args) {
 
-        int number, temp, lastDigit, total = 0;
+        int number, temp, lastDigit, total = 0, sumDigitsNumber = 0;
 
 
         System.out.print("Enter a number: ");
@@ -15,15 +15,20 @@ public class ArmstrongSayilariBul {
 
         for (int i = 1; i <= digitNumber; i++) {
             lastDigit = number % 10; // last digit || Sayının son hanesi
+            sumDigitsNumber += lastDigit; // sum of the digits of a number. || bir sayının rakamları toplamı.
             total += (int) Math.pow(lastDigit, digitNumber);
             number /= 10; // sayıyı 10'a bölerek son haneyi buluyoruz
         }
 
         if (temp == total) {
             System.out.println("Number is Armstrong Number");
+            System.out.println("Sayının rakamları toplamı: " + sumDigitsNumber);
+
         } else {
             System.out.println("Number isn't Armstrong Number");
+            System.out.println("Sayının rakamları toplamı: " + sumDigitsNumber);
         }
+
 
         scan.close();
     }
