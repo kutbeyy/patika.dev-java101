0076347f32b418089de0caeee581e927f55afba0
Fatih ÇELİK <info.fatihcelik@gmail.com>
2022-05-21 18:14:57 +0300

    Recursive Metotlar ile Asal Sayı kod düzeltildi bulma www.patika.dev

--- src/RecusivePrime.java
+++ src/RecusivePrime.java
@@ -4,7 +4,7 @@ public class RecusivePrime {
     private static final Scanner scan = new Scanner(System.in);
 
     public static boolean isPrime(int num, int div) {
-        if (num == 0 || num == 1) return false;
+        if (num <= 1) return false;
         if (div == 1) return true;
         if (num % div == 0) return false;
         return isPrime(num, div - 1);
